6b94414dcc14a47bebeebc3ef37851a574820a32
TheMorderous <estebanmagicoptm@gmail.com>
2022-05-18 20:44:20 -0400

    borra

--- IMC.js
+++ IMC.js
@@ -10,8 +10,9 @@ function calcular()
 {
     var  peso = document.getElementById("txtPeso").value;
     var  altura = document.getElementById("txtAltura").value;
+    var edad = document.getElementById (txtEdad).value;
 
-    if (peso >= 2 && peso <= 300 || altura >= 50 && altura <=250) {
+    if (peso >= 2 && peso <= 300 || altura >= 50 && altura <=250 || edad >= 20) {
         var newaltura = altura/100;
         var imc = peso / (newaltura * newaltura);
         alert("Su IMC es: " + imc.toFixed(2));
